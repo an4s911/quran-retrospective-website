a213788213492b5639d396b490912112f070249f
Anas Basheer <nasbas23@gmail.com>
2022-02-12 18:12:06 +0300

    Added checkbox which cuts out text when checked

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const thingsToLearn = document.querySelectorAll(".thing-to-learn");
+const thingsToLearn = document.querySelectorAll(".thing-to-learn > span");
 thingsToLearn.forEach((thing) => {
     thing.addEventListener("click", (event) => {
         const element = event.target;
@@ -7,5 +7,20 @@ thingsToLearn.forEach((thing) => {
             return;
         }
         element.textContent = newValue;
+    });
+});
+
+const checkBoxes = document.querySelectorAll(".thing-to-learn > input");
+checkBoxes.forEach((check) => {
+    check.addEventListener("change", (changeEvent) => {
+        const elem = changeEvent.target;
+        const thingToLearn = elem.parentElement.querySelector("span");
+        if (elem.checked) {
+            thingToLearn.style.color = "gray";
+            thingToLearn.style.textDecoration = "line-through";
+        } else {
+            thingToLearn.style.color = "initial";
+            thingToLearn.style.textDecoration = "initial";
+        }
     });
 });
