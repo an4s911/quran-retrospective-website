0b00a3412941f0b2e496fc79010f00451aa0f361
Anas Basheer <nasbas23@gmail.com>
2022-02-13 15:51:37 +0300

    Automatically generate boxes with dummy data
    Now I need to build a backend and make javascript fetch data from the
    backend and generate boxes automatically with actual data

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,91 @@
+/* GET DATA AND DISPLAY */
+const data = {
+    'Sun, 13 Feb': [
+        {
+            text: '',
+            plusStage: -1,
+            done: false,
+        },
+        {
+            text: '4qh2',
+            plusStage: 0,
+            done: false,
+        },
+        {
+            text: '3qh2',
+            plusStage: -1,
+            done: false,
+        }
+    ],
+    'Mon, 14 Feb': [
+        {
+            text: '',
+            plusStage: -1,
+            done: false,
+        },
+        {
+            text: '4qh1',
+            plusStage: 0,
+            done: false
+        },
+        {
+            text: '4qh2',
+            plusStage: 1,
+            done: false
+        }
+    ],
+}
+
+const boxesDiv = document.querySelector('.boxes');
+for (let item in data) {
+    const newBox = makeBoxHtml(item, data[item]);
+    boxesDiv.appendChild(newBox);
+}
+
+function makeBoxHtml(title, otherDetails) {
+    // makes a box of the format:
+    // <ul class="box">
+    //     <li class="day">
+    //         <!-- Day or Date -->
+    //         <h4>Sat, 12 Feb</h4>
+    //     </li>
+    //     <li class="thing-to-learn"><input type="checkbox" name="revised" id="">
+    //         <span>1HP19+0</span>
+    //     </li>
+    //     <li class="thing-to-learn"><input type="checkbox" name="revised" id="">
+    //         <span>4HQ58+3</span>
+    //     </li>
+    //     <li class="thing-to-learn"><input type="checkbox" name="revised" id="">
+    //         <span>2HQ1+1</span>
+    //     </li>
+    // </ul>
+    // otherDetails will be an array (size 3) of objects
+    // where each object has properties text: String, 
+    // plusStage: Number and done: bool
+    let element = document.createElement('ul');
+    element.classList.add('box')
+
+    let html = "";
+    html += `<li class='day'><h4>${title}</h4></li>`;
+
+    for (let i = 0; i < otherDetails.length; i++) {
+        html += `<li class='thing-to-learn'><input type='checkbox'`;
+        if (otherDetails[i].done) {
+            html += ` checked`
+        }
+        html += `>`
+        html += `<span>${otherDetails[i].text.toUpperCase()}`;
+        if (otherDetails[i].plusStage >= 0) {
+            html += `+${otherDetails[i].plusStage}`;
+        }
+        html += '</span></li>'
+    }
+
+    element.innerHTML = html;
+
+    return element;
+}
+
 const thingsToLearn = document.querySelectorAll(".thing-to-learn > span");
 thingsToLearn.forEach((thing) => {
     thing.addEventListener("click", (event) => {
