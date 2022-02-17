5100f261e5b736cf196c3ddeb60db4e25912e25f
Anas Basheer <nasbas23@gmail.com>
2022-02-17 19:03:33 +0300

    Generates even better random dummy data
    - Also set favicon

--- backend/app.py
+++ backend/app.py
@@ -1,47 +1,52 @@
 #!.venv/bin/python
 
+import random
+
 from datetime import datetime, timedelta
 from flask import Flask, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+date = datetime(year=2022, month=2, day=13)
+data = {}
+# Data's Format:
+# {
+#     "2022/02/12": [
+#         {
+#             "text": "1hp17",
+#             "plusStage": 2,
+#             "done": False,
+#         },
+#         {
+#             "text": "3qh2",
+#             "plusStage": -1,
+#             "done": True,
+#         },
+#         {
+#             "text": "1qh2",
+#             "plusStage": -1,
+#             "done": False,
+#         },
+#     ]
+# }
+plus_states = [-1, 0, 1, 3]
+for _ in range(100):
+    curr_data = []
+    for i in range(3):
+        temp_data = {}
+        temp_data['text'] = f'{random.randint(1, 4)}qh{random.randint(1, 60)}'
+        temp_data['plusStage'] = random.choice(plus_states)
+        temp_data['done'] = bool(random.getrandbits(1))
+        curr_data.append(temp_data)
+    date_formated = date.strftime('%Y/%m/%d')
+    data[date_formated] = curr_data
+    date += timedelta(days=1)
+
 
 @app.route("/api/getlist", methods=['POST'])
 def getlist():
-    # {
-    #     "2022/02/12": [
-    #         {
-    #             "text": "1hp17",
-    #             "plusStage": 2,
-    #             "done": False,
-    #         },
-    #         {
-    #             "text": "3qh2",
-    #             "plusStage": -1,
-    #             "done": True,
-    #         },
-    #         {
-    #             "text": "1qh2",
-    #             "plusStage": -1,
-    #             "done": False,
-    #         },
-    #     ],
-    # }
-    date = datetime(year=2022, month=2, day=13)
-    data = {}
-    for _ in range(100):
-        curr_data = []
-        for i in range(3):
-            temp_data = {}
-            temp_data['text'] = '1hp17'
-            temp_data['plusStage'] = -1
-            temp_data['done'] = True
-            curr_data.append(temp_data)
-        date_formated = date.strftime('%Y/%m/%d')
-        data[date_formated] = curr_data
-        date += timedelta(days=1)
-
+    global data
     return jsonify(data)
 
 
