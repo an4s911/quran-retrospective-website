8942815bba2fcf9e5689c9c868deebdc9c5185e5
Anas Basheer <nasbas23@gmail.com>
2022-02-20 10:55:27 +0300

    No more dummy data, data is stored in json file

--- backend/app.py
+++ backend/app.py
@@ -1,59 +1,57 @@
 #!.venv/bin/python
 
-import random
+import json
 import os
 
-from datetime import datetime, timedelta
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from dotenv import load_dotenv
+
 app = Flask(__name__)
+
 # So it supports Cross-origin stuff in javascript.
 # If this isn't here, then there is an error when using localhost
 CORS(app)
 
 load_dotenv()
 
-date = datetime(year=2022, month=2, day=13)
-data = {}
-# Data's Format:
-# {
-#     "2022/02/12": [
-#         {
-#             "text": "1hp17",
-#             "plusStage": 2,
-#             "done": False,
-#         },
-#         {
-#             "text": "3qh2",
-#             "plusStage": -1,
-#             "done": True,
-#         },
-#         {
-#             "text": "1qh2",
-#             "plusStage": -1,
-#             "done": False,
-#         },
-#     ]
-# }
-plus_states = [-1, 0, 1, 3]
-for _ in range(100):
-    curr_data = []
-    for i in range(3):
-        temp_data = {}
-        temp_data['text'] = f'{random.randint(1, 4)}QH{random.randint(1, 60)}'
-        temp_data['plusStage'] = random.choice(plus_states)
-        temp_data['done'] = bool(random.getrandbits(1))
-        curr_data.append(temp_data)
-    date_formated = date.strftime('%Y/%-m/%-d')
-    data[date_formated] = curr_data
-    date += timedelta(days=1)
+try:
+    data_file = open('data.json')
+except FileNotFoundError:
+    data = {}
+    with open('data.json', 'w') as data_file:
+        json.dump(data, data_file)
+else:
+    data = json.load(data_file)
+    data_file.close()
+    # Data's Format:
+    # {
+    #     "2022/02/12": [
+    #         {
+    #             "text": "1hp17",
+    #             "plusStage": 2,
+    #             "done": False,
+    #         },
+    #         {
+    #             "text": "3qh2",
+    #             "plusStage": -1,
+    #             "done": True,
+    #         },
+    #         {
+    #             "text": "1qh2",
+    #             "plusStage": -1,
+    #             "done": False,
+    #         },
+    #     ]
+    # }
 
 
 @app.route("/api/getlist", methods=['POST'])
 def getlist():
     global data
-    return jsonify(data)
+    with open('data.json') as data_file:
+        data = json.load(data_file)
+        return jsonify(data)
 
 
 @app.route('/api/updatedata', methods=["POST"])
@@ -67,6 +65,8 @@ def updateData():
         secret_key = temp_data.pop('secret_key')
         if secret_key == os.getenv('SECRET_KEY'):
             data = temp_data
+            with open('data.json', 'w') as data_file:
+                json.dump(data, data_file)
             return jsonify(success=True), 200
         else:
             return jsonify(success=False), 403
